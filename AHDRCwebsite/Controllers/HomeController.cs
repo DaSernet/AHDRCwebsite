ef758dd6d98f9d0d515c00b7bd1d173870b555b1
DaSernet <daan.seijnhaeve@tobania.be>
2021-03-12 22:32:55 +0100

    Added all homepages and fixed navbar issues

--- AHDRCwebsite/Controllers/HomeController.cs
+++ AHDRCwebsite/Controllers/HomeController.cs
@@ -28,6 +28,39 @@ namespace AHDRCwebsite.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Archive()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Disclaimer()
+        {
+            return View();
+        }
+        public IActionResult Help()
+        {
+            return View();
+        }
+        public IActionResult News()
+        {
+            return View();
+        }
+        public IActionResult Partnerships()
+        {
+            return View();
+        }
+        public IActionResult Terms()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
