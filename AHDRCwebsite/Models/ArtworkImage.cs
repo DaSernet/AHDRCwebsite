77c20585f4368bc72a3819862083923f4a0adc19
DaSernet <daanseijnhaeve@gmail.Com>
2022-07-29 13:46:20 +0200

    image properties

--- AHDRCwebsite/Models/ArtworkImage.cs
+++ AHDRCwebsite/Models/ArtworkImage.cs
@@ -4,6 +4,13 @@
     {
         public int Id { get; set; }
         public string ImageURL { get; set; }
+
+        public string Courtesy { get; set; }
+        public string Photographer { get; set; }
+
+        public string Copyright { get; set; }
+
+        public string ImageSize { get; set; }
         public Artwork Artwork { get; set; }
     }
 }
